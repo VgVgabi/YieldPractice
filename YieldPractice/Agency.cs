d04fadbeb88754f8f6ec3c1096bc994c968dd42f
vr6we <vr6we@LAPTOP-RC84REDT>
2022-03-13 18:55:21 +0200

    Add project files.

---
+++ YieldPractice/Agency.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+
+namespace YieldPractice
+{
+    internal class Agency
+    {
+        public IEnumerator GetEnumerator()
+        {
+            foreach (Car car in Cars)
+            {                
+                yield return car;                
+            }
+        }
+        //
+        public IEnumerable<Car> Cars;                        
+        public IEnumerable GetCars(int modelYear) {
+            foreach (Car car in Cars)
+            {
+                if (car.ModelYear == modelYear )
+                    yield return car;
+            }
+        }   
+        public IEnumerable GetCars(string maker) {
+            foreach (Car car in Cars)
+            {
+                if (car.Maker == maker)
+                    yield return car;
+            }
+        }     
+    }
+}
+
---
+++ YieldPractice/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace YieldPractice
+{
+    internal class Car
+    {
+        public double ModelYear { get; set; }
+
+        public string Maker { get; set; }
+        //
+        public override string ToString()
+        {
+            return ModelYear + " " + Maker;
+        }
+    }
+}
